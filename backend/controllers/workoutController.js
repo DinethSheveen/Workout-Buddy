5de6de6b304233001ff4f0aca8cd41879152dc0a
DinethSheveen <fdineth4000@gmail.com>
2025-11-13 09:29:17 +0000

    EXPRESS : Retreive all workout and a workout by id

--- backend/controllers/workoutController.js
+++ backend/controllers/workoutController.js
@@ -1,11 +1,34 @@
 import workoutModel from "../models/workoutModel.js"
 
+// GET WORKOUT BY ID
 export const getOneWorkout = async(req,res)=>{
+    const {workoutId} = req.params
     
+    if(!workoutId){
+        return res.status(400).send("Missing workout id")
+    }
+
+    try {
+        const workout = await workoutModel.findById(workoutId)
+
+        res.status(200).json({msg : "Workout retreived",data:workout})
+    } catch (error) {
+        res.status(500).send(error.message)
+    }
 }
+
+// GET ALL WORKOUTS 
 export const getAllWorkouts = async(_,res)=>{
-    
+    try {
+        const workout = await workoutModel.find()
+
+        res.status(200).json({msg : "All workouts retreived",data:workout})
+    } catch (error) {
+        res.status(500).send(error.message)
+    }
 }
+
+// CREATE A WORKOUT 
 export const createWorkout = async(req,res)=>{
     try {
         const {title,reps,load} = req.body
@@ -22,9 +45,13 @@ export const createWorkout = async(req,res)=>{
         res.status(500).send(error.message)
     }
 }
+
+// UPDATE A WORKOUT BY ID
 export const updateWorkout = async(req,res)=>{
     
 }
+
+// DELETE WORKOUT BY ID
 export const deleteWorkout = async(req,res)=>{
     
 }
