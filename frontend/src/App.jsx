6ee7ca9ebedf1e20f0f2a893a3ee1b5093c5ff59
DinethSheveen <fdineth4000@gmail.com>
2025-11-13 11:39:40 +0000

    VITE : Routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,17 @@
+import {BrowserRouter,Routes,Route} from "react-router-dom"
+import Navbar from "./components/Navbar"
+import Home from "./pages/Home"
+
 function App() {
   return (
-    <div>App</div>
+    <>
+      <BrowserRouter>
+        <Navbar/>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+        </Routes>
+      </BrowserRouter>
+    </>
   )
 }
 
