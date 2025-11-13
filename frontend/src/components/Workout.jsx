3d49a310684dbedc284d9a3e9d877afe3f6f8657
DinethSheveen <fdineth4000@gmail.com>
2025-11-13 17:36:33 +0000

    VITE : Deleted workout from the backend and rendered the workouts

--- frontend/src/components/Workout.jsx
+++ frontend/src/components/Workout.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import { RiDeleteBin5Line } from "react-icons/ri";
 
 function Workout({workout}) {
+
+  const deleteWorkout = async()=>{
+    const response = await fetch("http://localhost:3000/api/workouts/"+workout._id,
+      {
+        method : "DELETE"
+      }
+  )
+
+    const json = await response.json();
+    
+    if(response.ok){
+      console.log(json);
+    }
+    else{
+      console.log("JSON"+json);
+    }
+    
+  }
+
   return (
     <div className='bg-gray-400 mb-6 p-4 font-bold rounded-[10px] max-w-[70vw]'>
-        <p className='text-cyan-800 text-2xl'>{workout.title}</p>
+        <div className='flex justify-between items-center'>
+          <p className='text-cyan-800 text-2xl'>{workout.title}</p>
+          <RiDeleteBin5Line className="text-red-700 text-2xl cursor-pointer" onClick={deleteWorkout}/>
+        </div>
         <p className='text-white'>Reps : {workout.reps}</p>
         <p className='text-white'>Load (kg) : {workout.load}</p>
         <p className='text-white'>{workout.createdAt}</p>
