3bf778138a02defe2a91485ebb154ba1635c98f2
DinethSheveen <fdineth4000@gmail.com>
2025-11-13 14:55:25 +0000

    VITE : Position fixed

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -5,8 +5,8 @@ function Navbar() {
     // NAVBAR
     <div className="navbar">
         {/* FLEX CONTAINER */}
-        <div className="flex justify-between items-center text-white font-bold sticky w-full bg-cyan-700 px-10 py-5">
-            <Link to={"/"} className="text-2xl">Workout Buddy</Link>
+        <div className="flex justify-between items-center text-white bg-cyan-800 font-bold fixed w-full px-10 py-5">
+            <Link to={"/"} className="text-3xl">Workout Buddy</Link>
         </div>
     </div>
   )
