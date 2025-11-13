428ebae80e2ed8571cdb317a3cf066534d15eba3
DinethSheveen <fdineth4000@gmail.com>
2025-11-13 13:03:32 +0000

    VITE : Fetched workout details from the backend

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,44 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 function Home() {
+
+    const [workouts, setWorkout] = useState(null)
+
+    useEffect(()=>{
+        const fetchWorkouts = async()=>{
+            try {
+                const response = await fetch("http://localhost:3000/api/workouts")
+                
+                if(!response.ok){
+                    throw new Error("Couldn't fetch resources")
+                }
+                else{
+                    const data = await response.json()
+                    setWorkout(data);
+                }
+                
+
+            } catch (error) {
+                console.log(error.message);
+                
+            }
+        }
+        
+        fetchWorkouts()
+    },[])
+
   return (
-    <div>Home</div>
+    <div>
+        {workouts && workouts.map((workout)=>{
+            return(
+                <div key={workout._id}>
+                    <p>Workout - {workout.title}</p>
+                    <p>Reps : {workout.reps}</p>
+                    <p>Load : {workout.load}</p>
+                </div>
+            )
+        })}
+    </div>
   )
 }
 
