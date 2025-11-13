3ad06efcddfa9431c29665bb6cba7a4ec8727edc
DinethSheveen <fdineth4000@gmail.com>
2025-11-13 16:02:42 +0000

    VITE : Workout component

--- frontend/src/components/Workout.jsx
+++ frontend/src/components/Workout.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Workout({workout}) {
   return (
-    <div className='bg-gray-400 m-2 p-4 font-bold rounded-[10px] max-w-[70vw]'>
+    <div className='bg-gray-400 mb-6 p-4 font-bold rounded-[10px] max-w-[70vw]'>
         <p className='text-cyan-800 text-2xl'>{workout.title}</p>
         <p className='text-white'>Reps : {workout.reps}</p>
         <p className='text-white'>Load (kg) : {workout.load}</p>
