3e569165c2f05f5c67dbdb8471401cec22c51a17
DinethSheveen <fdineth4000@gmail.com>
2025-11-13 11:52:51 +0000

    VITE : Navbar structure

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,8 +2,12 @@ import { Link } from "react-router-dom"
 
 function Navbar() {
   return (
-    <div>
-        <Link to={"/"}>Workout Buddy</Link>
+    // NAVBAR
+    <div className="navbar">
+        {/* FLEX CONTAINER */}
+        <div className="flex justify-between items-center text-white font-bold sticky w-full bg-cyan-700 px-10 py-5">
+            <Link to={"/"} className="text-2xl">Workout Buddy</Link>
+        </div>
     </div>
   )
 }
