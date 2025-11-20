c8b0d269371a8ddc456e36e00fa252d1c63473b8
DinethSheveen <fdineth4000@gmail.com>
2025-11-20 15:07:19 +0000

    VITE : Login form

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import { Link } from "react-router-dom"
 
 function Login() {
+
   return (
-    <div className='pt-20'>Login</div>
+    <div className='pt-40 w-[80vw] mx-auto mb-10 max-h-100 md:w-[40vw]'>
+        <form className='form flex flex-col gap-5 justify-around h-full shadow-2xl shadow-gray-500 rounded-[10px] bg-gray-300 py-4 px-2'>
+        <p className="text-3xl font-bold text-cyan-900">Login</p>
+            {/* USERNAME */}
+            <div className='flex flex-col gap-2'>
+                <label htmlFor="title" className='font-bold'>Username</label>
+                <input type="text" id='title' placeholder='Username...' className='text-white bg-gray-400 p-2 rounded-[5px]'/>
+            </div>
+            
+            {/* PASSWORD */}
+            <div className='flex flex-col gap-2'>
+                <label htmlFor="reps" className='font-bold'>Password</label>
+                <input type="text" id='reps' placeholder='Password...' className='text-white bg-gray-400 p-2 rounded-[5px]'/>
+            </div>
+
+            <button className='bg-cyan-800 text-white mt-2 p-2 cursor-pointer hover:bg-cyan-700 active:bg-cyan-600 transition-all'>Login</button>
+        
+            <p>Do not have an account? <Link to={"/register"}  className="text-cyan-800 font-bold">Register</Link></p>
+
+        </form>
+        {/* {error && error? <div className='bg-red-200 border-red-600 border-2 text-red-600 font-bold py-2 px-4 rounded-[5px]'>!</div>:""}
+        {success && success? <div className='bg-green-200 border-green-600 border-2 text-green-600 font-bold py-2 px-4 rounded-[5px]'>! Redirecting...</div>:""} */}
+    </div>
   )
 }
 
