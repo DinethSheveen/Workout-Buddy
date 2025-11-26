fe098787bd15508c7169d069f7de84fdd7eb9434
DinethSheveen <fdineth4000@gmail.com>
2025-11-26 23:13:28 +0000

    VITE : NavLinks instead of Links

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react"
-import { useNavigate,Link } from "react-router-dom"
+import { useNavigate, NavLink } from "react-router-dom"
 import profileIcon from "../../public/logo.jpg"
 import { formatUsername } from "../utils/formatUsername"
 
@@ -31,26 +31,38 @@ function Navbar({authorizedUser,loggedIn}) {
     <div className="navbar">
         {/* FLEX CONTAINER */}
         <div className="fixed w-full text-white px-3 py-4 md:px-10">
-          <div className="flex justify-between gap-5 items-center font-bold border-2 border-white rounded-full py-2 px-4">
+          <div className="flex justify-between gap-5 items-center font-bold border-2 border-gray-400 rounded-full py-2 px-4">
             {/* LEFT SECTION */}
-            <p className="text-2xl cursor-pointer md:text-3xl" onClick={handleNavigate}>Workout Buddy</p>
+            <p className="text-2xl cursor-pointer md:text-3xl text-transparent bg-clip-text bg-linear-to-r from-[#696bcc] to-gray-500" onClick={handleNavigate}>Workout Buddy</p>
 
             {/* MIDDLE SECTION */}
             <div className="flex items-center gap-3">
-              <Link to={"/"}>Home</Link>
-              <Link to={"/my-workouts"}>My-Workouts</Link>
+              
             </div>
 
             {/* RIGHT SECTION */}
             <div>
               {
                 loggedIn?
-                <div className="flex flex-col gap-2">
-                  <div className="flex items-center gap-1 cursor-pointer" onClick={()=>navigate("/profile")}>
-                    <p className="text-transparent bg-linear-to-r from-gray-300 to-gray-400 bg-clip-text"> {name}</p>
-                    <img src={profileIcon} className="w-10 rounded-full h-10"/>
+                
+                  <div className="flex items-center gap-5 cursor-pointer">
+                    <NavLink to={"/"} className={({isActive})=>
+                      isActive? 
+                        "text-[#696bcc] text-[20px]" : ""
+                    }>
+                      Home
+                    </NavLink>
+                    <NavLink to={"/my-workouts"} className={({isActive})=>
+                      isActive?
+                        "text-[#696bcc] text-[20px]" : ""
+                    }>
+                      My-Workouts
+                    </NavLink>
+                    <div className="flex items-center" onClick={()=>navigate("/profile")}>
+                      <p className="text-transparent bg-linear-to-r from-gray-300 to-gray-400 bg-clip-text"> {name}</p>
+                      <img src={profileIcon} className="w-10 rounded-full h-10"/>
+                    </div>
                   </div>
-                </div>
                 :
               <p className="text-transparent bg-linear-to-r from-gray-300 to-gray-400 bg-clip-text">Create your own workout space</p>
               } 
