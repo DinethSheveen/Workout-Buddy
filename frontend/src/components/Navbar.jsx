e033311a35fdc8168dda717646748201429af10e
DinethSheveen <fdineth4000@gmail.com>
2025-11-26 11:39:12 +0000

    VITE : Updated navbar with page links

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react"
-import { useNavigate } from "react-router-dom"
+import { useNavigate,Link } from "react-router-dom"
 import profileIcon from "../../public/logo.jpg"
 import { formatUsername } from "../utils/formatUsername"
 
@@ -30,26 +30,33 @@ function Navbar({authorizedUser,loggedIn}) {
     // NAVBAR
     <div className="navbar">
         {/* FLEX CONTAINER */}
-        <div className="flex justify-between gap-5 items-center text-white bg-gray-800 font-bold fixed w-full px-3 py-5 md:px-10">
-          {/* LEFT SECTION */}
-          <p className="text-3xl cursor-pointer" onClick={handleNavigate}>Workout Buddy</p>
+        <div className="fixed w-full text-white px-3 py-4 md:px-10">
+          <div className="flex justify-between gap-5 items-center font-bold border-2 border-white rounded-full py-2 px-4">
+            {/* LEFT SECTION */}
+            <p className="text-2xl cursor-pointer md:text-3xl" onClick={handleNavigate}>Workout Buddy</p>
 
-          {/* RIGHT SECTION */}
-          
-          <div>
-            {
-              loggedIn?
-              <div className="flex flex-col gap-2">
-                <div className="flex items-center gap-1 cursor-pointer" onClick={()=>navigate("/profile")}>
-                  <p className="text-transparent bg-linear-to-r from-gray-300 to-gray-400 bg-clip-text"> {name}</p>
-                  <img src={profileIcon} className="w-10 rounded-full h-10"/>
+            {/* MIDDLE SECTION */}
+            <div className="flex items-center gap-3">
+              <Link to={"/"}>Home</Link>
+              <Link to={"/my-workouts"}>My-Workouts</Link>
+            </div>
+
+            {/* RIGHT SECTION */}
+            <div>
+              {
+                loggedIn?
+                <div className="flex flex-col gap-2">
+                  <div className="flex items-center gap-1 cursor-pointer" onClick={()=>navigate("/profile")}>
+                    <p className="text-transparent bg-linear-to-r from-gray-300 to-gray-400 bg-clip-text"> {name}</p>
+                    <img src={profileIcon} className="w-10 rounded-full h-10"/>
+                  </div>
                 </div>
-              </div>
-              :
-            <p className="text-transparent bg-linear-to-r from-gray-300 to-gray-400 bg-clip-text">Create your own workout space</p>
-            } 
+                :
+              <p className="text-transparent bg-linear-to-r from-gray-300 to-gray-400 bg-clip-text">Create your own workout space</p>
+              } 
+            </div>
           </div>
-        </div>
+          </div>
     </div>
   )
 }
---
+++ frontend/src/pages/MyWorkouts.jsx
@@ -0,0 +1,44 @@
+import { useEffect, useState } from 'react'
+import Workout from '../components/Workout'
+import WorkoutForm from '../components/WorkoutForm'
+
+function MyWorkouts() {
+
+    const [workouts, setWorkout] = useState(null)
+
+    useEffect(()=>{
+        const fetchWorkouts = async()=>{
+            try {
+                const id = JSON.parse(localStorage.getItem("user"))._id
+
+                const response = await fetch(`http://localhost:3000/api/workouts/user/${id}`)
+
+                if(!response.ok){
+                    throw new Error("Couldn't fetch resources")
+                }
+                else{
+                    const data = await response.json()
+                    setWorkout(data);                    
+                }
+            } catch (error) {
+                console.log(error.message);
+            }
+        }
+        fetchWorkouts()
+    },[workouts])
+
+  return (
+    <div className='flex flex-col-reverse justify-between gap-5 px-4 pt-35 md:pt-30 md:flex-row'>
+        <div className='flex-1'>
+            {workouts && workouts.map((workout)=>{
+                return(
+                    <Workout key={workout._id} workout={workout}/>
+                )
+            })}
+        </div>
+        <WorkoutForm/>
+    </div>
+  )
+}
+
+export default MyWorkouts
