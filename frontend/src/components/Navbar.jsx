6d526c38e9383dd7f282afd48b88bb353996434d
DinethSheveen <fdineth4000@gmail.com>
2025-11-28 13:01:23 +0000

    VITE : Responsive home page and navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -32,14 +32,11 @@ function Navbar({authorizedUser,loggedIn}) {
     <div className="navbar">
         {/* FLEX CONTAINER */}
         <div className="fixed w-full text-white px-3 py-4 md:px-10">
-          <div className="flex justify-between gap-5 items-center font-bold border-2 border-gray-400 rounded-full py-2 px-4">
+          {/* OUTER CONTAINER */}
+          <div className="2xl:mx-auto 2xl:max-w-[1400px] 2xl:py-5">
+            <div className="flex justify-between gap-5 items-center font-bold border-2 border-gray-400 rounded-full py-2 px-4 2xl:px-8 2xl:text-4xl 2xl:border-4">
             {/* LEFT SECTION */}
-            <p className="text-2xl cursor-pointer md:text-3xl text-transparent bg-clip-text bg-linear-to-r from-[#696bcc] to-gray-500" onClick={handleNavigate}>Workout Buddy</p>
-
-            {/* MIDDLE SECTION */}
-            <div className="flex items-center gap-3">
-              
-            </div>
+            <p className="text-2xl cursor-pointer md:text-3xl text-transparent bg-clip-text bg-linear-to-r from-[#696bcc] to-gray-500 2xl:py-3 2xl:text-5xl" onClick={handleNavigate}>Workout Buddy</p>
 
             {/* RIGHT SECTION */}
             <div>
@@ -50,13 +47,13 @@ function Navbar({authorizedUser,loggedIn}) {
                     <div className="hidden md:flex md:items-center md:gap-5 md:cursor-pointer">
                       <NavLink to={"/"} className={({isActive})=>
                         isActive? 
-                          "text-[#696bcc] text-[20px]" : ""
+                          "text-[#696bcc]" : ""
                       }>
                         Home
                       </NavLink>
                       <NavLink to={"/my-workouts"} className={({isActive})=>
                         isActive?
-                          "text-[#696bcc] text-[20px]" : ""
+                          "text-[#696bcc]" : ""
                       }>
                         My-Workouts
                       </NavLink>
@@ -71,6 +68,7 @@ function Navbar({authorizedUser,loggedIn}) {
               <p className="text-transparent bg-linear-to-r from-gray-300 to-gray-400 bg-clip-text">Create your own workout space</p>
               } 
             </div>
+          </div>
           </div>
           </div>
     </div>
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,25 +2,24 @@ import {Link} from "react-router-dom"
 
 function Home() {
   return (
-    <div className="pt-30 text-gray-300 px-5 md:px-0 md:pt-40">
+    <div className="text-gray-300 px-5 md:px-0">
       {/* HEADER */}
-      <div className="flex flex-col justify-center items-center gap-5">
+      <div className="flex flex-col justify-center items-center gap-5 min-h-screen 2xl:gap-15">
         <div>
-          <p className="uppercase text-[#696bcc] text-2xl font-bold rounded-full border-2 border-[#696bcc] px-4 py-2 bg-white/10 backdrop-blur-3xl">Fitness for life</p>
+          <p className="uppercase text-[#696bcc] text-2xl font-bold rounded-full border-2 border-[#696bcc] px-4 py-2 bg-white/10 backdrop-blur-3xl md:text-3xl lg:text-4xl xl:px-8 xl:py-4 xl:border-4 2xl:text-7xl">Fitness for life</p>
         </div>
         {/* HEADING */}
-          <div className="md:w-[65%]">
-          <p className="text-4xl font-bold text-center md:text-5xl">Elevate Your <span className="text-transparent bg-clip-text bg-linear-to-r from-purple-300 to-[#696bcc]">Fitness</span> Journey with <span className="text-transparent bg-clip-text bg-linear-to-r from-purple-300 to-[#696bcc]">Workout Buddy</span>⚡</p>
+          <div className="md:w-[65%] 2xl:w-[65%]">
+          <p className="text-4xl font-bold text-center 2xl:text-8xl">Elevate Your <span className="text-transparent bg-clip-text bg-linear-to-r from-purple-300 to-[#696bcc]">Fitness</span> Journey with <span className="text-transparent bg-clip-text bg-linear-to-r from-purple-300 to-[#696bcc]">Workout Buddy</span>⚡</p>
         </div>
         {/* SUB-HEADING */}
-        <div className="text-2xl font-bold text-gray-400 text-center md:w-[40%]">
+        <div className="text-2xl font-bold text-gray-400 text-center md:w-[40%] 2xl:w-[45%] 2xl:text-5xl">
           <p>Unlock Your Potential with Our Personalized Fitness Ecosystem.</p>
         </div>
-        <div>
-          <div className="flex flex-col items-center jusitfy-between gap-4 font-bold md:flex-row md:gap-0 md:rounded-none">
-            <Link to={"/my-workouts"} className="bg-gray-600 px-6 py-3 md:py-4 md:px-12 rounded-[10px] md:rounded-l-[10px] md:rounded-none">My Workouts</Link>
-            <Link to={"/my-workouts"} className="bg-[#5759d4] px-6 py-3 md:py-4 md:px-12 rounded-[10px] md:rounded-r-[10px] md:rounded-none">Get Started</Link>
-          </div>
+        {/* ACTION BUTTONS */}
+        <div className="flex flex-col items-center jusitfy-between gap-4 font-bold md:flex-row md:gap-0 md:rounded-none 2xl:text-6xl">
+          <Link to={"/my-workouts"} className="bg-gray-600 px-6 py-3 md:py-4 md:px-12 rounded-[10px] md:rounded-l-[10px] md:rounded-none 2xl:py-6 2xl:px-12 2xl:rounded-l-[15px]">My Workouts</Link>
+          <Link to={"/my-workouts"} className="bg-[#5759d4] px-6 py-3 md:py-4 md:px-12 rounded-[10px] md:rounded-r-[10px] md:rounded-none 2xl:py-6 2xl:px-12 2xl:rounded-r-[15px]">Get Started</Link>
         </div>
       </div>
     </div>
